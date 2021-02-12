747f66161b2edd4e2d5a5fc10ca291cab368056c
Verde <Verde@Virtualbox>
2021-02-12 10:41:21 -0500

    First introduction

---
+++ example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def print_args(a, b , c, d):
+
+  print("Hello World from python")
+  print("--- Received {}".format(a))
+  print("--- Received {}".format(b))
+  print("--- Received {}".format(c))
+  print("--- Received {}".format(d))
+
+def no_args():
+
+  return bytearray(b'This is a function with no args')
