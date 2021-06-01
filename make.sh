f39545f05a2f9351d0523dca4bd6ad01d786367b
Verde <Verde@knowhere>
2021-06-01 19:22:17 -0400

    Forgot to delete these

--- make.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-g++ -std=c++17 -ggdb3 -O0 -pedantic-errors -Wall \
-    -fpie $(python3.8-config --cflags --embed) -o 'example.out' \
-    'main.cpp' $(python3.8-config --embed --ldflags)
